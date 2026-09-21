73e9bb705b9efb21ef69ecf7f56aad49e99da038
altradits <altradits@gmail.com>
2026-09-21 14:15:27 +0300

    feat(channels): add Zambia cash payment channel

---
+++ src/channels/zm/cash.ts
@@ -0,0 +1,7 @@
+import { createCashPaymentChannel } from "../shared.js";
+
+export const cashZmZmwPaymentChannel = createCashPaymentChannel({
+  id: "cash_zm_zmw",
+  country: "ZM",
+  currency: "ZMW",
+});
